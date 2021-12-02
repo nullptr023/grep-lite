3f962ba19f6055dfa48babed40c8673af1e0d43a
Austin Villanueva <programmer9323@gmail.com>
2021-12-02 07:54:14 -0600

    add clap crate and add pattern for regex

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use regex::Regex;
+use clap::{App, Arg};
+
 
 fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("The pattern to search for")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+     
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
-    let re = Regex::new("picture").unwrap();
     let  quote = "Every face, every shop, bedroom window, public-house,
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of pages";
